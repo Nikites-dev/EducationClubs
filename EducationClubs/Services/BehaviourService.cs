4b64e1495f2b35f62895eca5758360ab0497fb7b
Nikites <rockstar.gg@mail.ru>
2023-10-15 19:06:28 +0300

    add auth user

---
+++ EducationClubs/Services/BehaviourService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using EducationClubs.ScaffoldedModels;
+
+namespace EducationClubs.Services
+{
+    public class BehaviourService
+    {
+        public Account? authAccount { get; set; }
+    }
+}
